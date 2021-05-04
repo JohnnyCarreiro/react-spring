843d75d4fd0c9f038b579ccc7a83abe16a57401a
Johnny <jcarreiropublicidade@gmail.com>
2021-05-04 17:04:38 -0300

    feat: Create first animation using react springs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,32 @@
-import Head from 'next/head'
+import { useState } from 'react'
+import { animated, useTransition } from 'react-spring'
+
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
+  const [isVisible, setIsVisible] = useState(false)
+  const transition = useTransition(isVisible, {
+    from: { x: -100, y: 800, opacity: 0 },
+    enter: { x: 0, y: 0, opacity: 1 },
+    leave: { x: 100, y: 800, opacity: 0 },
+  })
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Template Next Tipescript</title>
-      </Head>
-      <section>
-        <h1>Template using typescript and Sass</h1>
-      </section>
+    <div className={styles.app}>
+      <button
+        type="button"
+        className={styles.button}
+        onClick={() => {
+          setIsVisible((visible) => !visible)
+        }}
+      >
+        {isVisible ? 'Un-mont' : 'Mount'}
+      </button>
+      <div className={styles.container}>
+        {transition((style, item)=>
+          item ? <animated.div style={style} className={styles.item} /> : ''
+        )}
+      </div>
     </div>
   )
 }
